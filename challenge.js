3a33a7ac4b8e91aa92401b873a672dfe1ca41292
Atibakh <a.bakherad1991@gmail.com>
2023-01-16 12:39:49 -0500

    update challenge

--- challenge.js
+++ challenge.js
@@ -1,5 +1,7 @@
 //challenge 2 final position
 
+/*
+
 const moves = ["north", "north", "west", "west", "north", "east", "north"];
 
 const finalPosition = function (moves) {
@@ -75,4 +77,56 @@ console.log(calculateTriangleArea(10, -5)); // should print undefined
 console.log(calculateCircleArea(10)); // should print 314.159...
 console.log(calculateCircleArea(3.5)); // should print 38.484...
 console.log(calculateCircleArea(-1)); // should print undefined
+
+
+
+//challenge looping-range
+function range (start, end, step){
+  
+let arrayNum = [];
+if(start === Number || end === Number || step === Number) return arrayNum = [];
+if(start > end) return arrayNum = [];
+if(step <= 0) return arrayNum = [];
+for(let i = start; i <= end; i++){
+  if(i % step === 0){
+    arrayNum.push(i)
+  }
+}
+
+return arrayNum;
+
+
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+
+
+
+// challenhe looping lastIndexOf
+
+function lastIndexOf(arr, val){
+  const index = arr.lastIndexOf(val);
+  return index;
+
+};
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+*/
+
+//challenge concatenate arrays
+
+function concat (arr1, arr2){
+  return arr1.concat(arr2);
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
 
